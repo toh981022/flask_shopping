234b6595879092018d6cfca6be10c97a471a2392
toh981022 <2023jfr-ic-student002@la-study.com>
2023-05-25 16:37:28 +0900

    初回コミット

--- admin/config.py
+++ admin/config.py
@@ -1,5 +1,2 @@
 # デバッグモードをONにする
 DEBUG = True
-# ログイン情報を設定
-ADMIN_USER_ID = 'admin'
-ADMIN_PASSWORD = 'adpassword'
--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,4 +1,2 @@
 from .category import Category
 from .item import Item
-from .order import Order
-from .customer import Customer
